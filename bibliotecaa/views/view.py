24083bcea552433f73882af0abb5fabfb12bd12f
Rodrigo <05rotapi@gmail.com>
2025-02-13 08:58:36 +0100

    init

---
+++ bibliotecaa/views/view.py
@@ -0,0 +1,11 @@
+def menu():
+    opcion = input("""  1. Alta de socio
+                        2. Baja de socio
+                        3. Alta de libro
+                        4. Baja de libro
+                        5. Prestar libro
+                        6. Devolver libro
+                        7. Consultar libros
+                        8. Consultar usuarios
+                        9. Consultar prestamos
+                        0. Salir""")
